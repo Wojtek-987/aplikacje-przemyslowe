9a20172fc8a4c915ab6bd7e86b5ae363f2d24742
Wojtek987 <maciej.woj05@gmail.com>
2025-10-03 12:22:15 +0200

    first commit

---
+++ demo/src/test/java/com/aplikacje_przemyslowe_lab01/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplikacje_przemyslowe_lab01.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
